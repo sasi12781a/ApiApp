f5fb85dcd8af7c34ea7dda3e07b601084952f1e1
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-07-07 13:15:21 +0530

    m

---
+++ Src/Home.js
@@ -0,0 +1,19 @@
+import React,{useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  View,
+  ScrollView,Text,Button
+} from 'react-native';
+
+
+
+const Home=()=>{
+  return(
+    <View style={{justifyContent:'center',alignItems:"center",flex:1}}>
+        <Text style={{margin:20}}>
+            sasi
+        </Text>
+    </View>
+  )
+}
+export default Home;
